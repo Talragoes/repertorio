a5c1d262c7716a21537ac5abb594c1380ba77839
Talragoes <129749253+Talragoes@users.noreply.github.com>
2024-06-25 09:31:16 -0300

    upload index

---
+++ home.php
@@ -0,0 +1,142 @@
+<div class="base-banner limpo alt bn-img">
+<div class="glider-contain">
+    <div class="glider">
+            <div><span class="slide-title"><img src="img/banner01.png" alt=""></span></div>
+            <div><span class="slide-title"><img src="img/banner02.png" alt=""></span></div>
+            <div><span class="slide-title"><img src="img/banner03.png" alt=""></span></div>
+        </div>
+        <button class="glider-prev">&laquo;</button>
+        <button class="glider-next">&raquo;</button>
+        <div id="dots"></div>
+    </div>
+</div>
+<div class="conteudo">
+	<div class="home">
+		<div class="home-filtro">
+			<form action="">
+				<div class="rows center-middle">
+					<div class="col-4"><span class="text-label">Nome do evento</span><input type="text" class="form-campo"></div>
+					<div class="col-2">
+						<span class="text-label">Estado</span>
+						<select class="form-campo">
+							<option value="MA">MA</option>
+							<option value="SP">SP</option>
+						</select>
+					</div>
+					<div class="col-4">
+						<div class="d-flex" style="gap:10px">
+							<div class="width-100">
+								<span class="text-label">Data inicial</span>
+								<input type="date" class="form-campo">
+							</div>
+							<div class="width-100">
+								<span class="text-label">Data final</span>
+								<input type="date" class="form-campo">
+							</div>
+						</div>
+						
+					</div>
+					
+					<div class="col mt-2"><input type="button" class="btn btn-roxo width-100" value="Filtrar"></div>
+				</div>
+			</form>
+		</div>
+
+		<div class="rows listaInscricoes">
+			<div class="col-4 mb-4 d-flex">
+				<div class="caixa">
+					<div class="thumb">
+						<img src="img/banner01.png" alt="" class="img-fluido">
+					</div>
+					<strong>Maratona Brasília 2024</strong>
+					<div class="d-flex justify-content-between datas">
+						<span class="d-flex center-middle"><i class="ico ico-date"></i> 20/04/2024</span>
+						<span class="d-flex center-middle"><i class="ico ico-local"></i> Anapolis - GO</span>
+					</div>
+					<div class="p-2">
+						<a href="index.php?link=2" class="btn btn-verde d-flex center-middle justify-content-center" style="gap:10px"><i class="fas fa-arrow-right h4 mb-0"></i> Inscrição</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-4 mb-4 d-flex">
+				<div class="caixa">
+					<div class="thumb">
+						<img src="img/banner02.png" alt="" class="img-fluido">
+					</div>
+					<strong>Corrida e caminhada da UEMA</strong>
+					<div class="d-flex justify-content-between datas">
+						<span class="d-flex center-middle"><i class="ico ico-date"></i> 20/04/2024</span>
+						<span class="d-flex center-middle"><i class="ico ico-local"></i> Anapolis - GO</span>
+					</div>
+					<div class="p-2">
+						<a href="index.php?link=2" class="btn btn-verde d-flex center-middle justify-content-center" style="gap:10px"><i class="fas fa-arrow-right h4 mb-0"></i> Inscrição</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-4 mb-4 d-flex">
+				<div class="caixa">
+					<div class="thumb">
+						<img src="img/banner03.png" alt="" class="img-fluido">
+					</div>
+					<strong>Corrida projeto policial guerreiros</strong>
+					<div class="d-flex justify-content-between datas">
+						<span class="d-flex center-middle"><i class="ico ico-date"></i> 20/04/2024</span>
+						<span class="d-flex center-middle"><i class="ico ico-local"></i> Anapolis - GO</span>
+					</div>
+					<div class="p-2">
+						<a href="index.php?link=2" class="btn btn-verde d-flex center-middle justify-content-center" style="gap:10px"><i class="fas fa-arrow-right h4 mb-0"></i> Inscrição</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-4 mb-4 d-flex">
+				<div class="caixa">
+					<div class="thumb">
+						<img src="img/banner03.png" alt="" class="img-fluido">
+					</div>
+					<strong>Corrida Projeto Policial Guerreiros</strong>
+					<div class="d-flex justify-content-between datas">
+						<span class="d-flex center-middle"><i class="ico ico-date"></i> 20/04/2024</span>
+						<span class="d-flex center-middle"><i class="ico ico-local"></i> Anapolis - GO</span>
+					</div>
+					<div class="p-2">
+						<a href="index.php?link=2" class="btn btn-verde d-flex center-middle justify-content-center" style="gap:10px"><i class="fas fa-arrow-right h4 mb-0"></i> Inscrição</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-4 mb-4 d-flex">
+				<div class="caixa">
+					<div class="thumb">
+						<img src="img/banner02.png" alt="" class="img-fluido">
+					</div>
+					<strong>Corrida e caminhada da UEMA</strong>
+					<div class="d-flex justify-content-between datas">
+						<span class="d-flex center-middle"><i class="ico ico-date"></i> 20/04/2024</span>
+						<span class="d-flex center-middle"><i class="ico ico-local"></i> Anapolis - GO</span>
+					</div>
+					<div class="p-2">
+						<a href="index.php?link=2" class="btn btn-verde d-flex center-middle justify-content-center" style="gap:10px"><i class="fas fa-arrow-right h4 mb-0"></i> Inscrição</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-4 mb-4 d-flex">
+				<div class="caixa">
+					<div class="thumb">
+						<img src="img/banner01.png" alt="" class="img-fluido">
+					</div>
+					<strong>Maratona Brasília 2024</strong>
+					<div class="d-flex justify-content-between datas">
+						<span class="d-flex center-middle"><i class="ico ico-date"></i> 20/04/2024</span>
+						<span class="d-flex center-middle"><i class="ico ico-local"></i> Anapolis - GO</span>
+					</div>
+					<div class="p-2">
+						<a href="index.php?link=2" class="btn btn-verde d-flex center-middle justify-content-center" style="gap:10px"><i class="fas fa-arrow-right h4 mb-0"></i> Inscrição</a>
+					</div>
+				</div>
+			</div>
+		
+		</div>
+		
+	</div>
+</div>
+
